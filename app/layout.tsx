c61c94c3558cd4922421c16bc41e5d68437c410a
Lucas Deng <lucaszz@Lucass-MacBook-Pro.local>
2025-03-01 16:32:48 -0600

    Fix a typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import NavBar from "./navbar";
+import NavBar from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
