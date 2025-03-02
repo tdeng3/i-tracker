a22116b043e02b088cfe1b236e3a69382468b64c
Lucas Deng <lucaszz@Lucass-MacBook-Pro.local>
2025-03-01 18:38:21 -0600

    Setup Radix UI theme

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const issuePage = () => {
-  return <div>issuePage</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default issuePage;
