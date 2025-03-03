3e6c2f3cf56f6bc741315b7c9f976d3a6ee01a46
Lucas Deng <lucaszz@Lucass-MacBook-Pro.local>
2025-03-03 16:22:43 -0600

    Implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,39 @@
 "use client";
-import { Button, Callout, TextField, TextFieldInput } from "@radix-ui/themes";
+import {
+  Button,
+  Callout,
+  Text,
+  TextField,
+  TextFieldInput,
+} from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
 
 const NewIssuePage = () => {
   const [error, setError] = useState("");
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   return (
     <div className="max-w-xl">
       {error && (
@@ -41,6 +59,11 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextFieldInput placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -48,7 +71,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         {/*SimpleMDE https://www.npmjs.com/package/react-simplemde-editor#install*/}
         <Button>Submit new issue</Button>
       </form>
