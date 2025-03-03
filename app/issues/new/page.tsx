5bcd022ca2bce7b66f1ae35073fd8d67984df31d
Lucas Deng <lucaszz@Lucass-MacBook-Pro.local>
2025-03-03 16:00:15 -0600

    Handle error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { Button, TextField, TextFieldInput } from "@radix-ui/themes";
+import { Button, Callout, TextField, TextFieldInput } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface IssueForm {
   title: string;
@@ -12,33 +13,46 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+  const [error, setError] = useState("");
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      {/*max-w-xl: it makes the width to extra large*/}
-      {/*space-y-3: it gives 3px space Verticaly between each items within this form tag*/}
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-3">
+          <Callout.Text color="red">{error}</Callout.Text>
+        </Callout.Root>
+      )}
 
-      <TextField.Root>
-        <TextFieldInput placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occurred!");
+          }
+        })}
+      >
+        {/*max-w-xl: it makes the width to extra large*/}
+        {/*space-y-3: it gives 3px space Verticaly between each items within this form tag*/}
 
-      {/*SimpleMDE https://www.npmjs.com/package/react-simplemde-editor#install*/}
-      <Button>Submit new issue</Button>
-    </form>
+        <TextField.Root>
+          <TextFieldInput placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        {/*SimpleMDE https://www.npmjs.com/package/react-simplemde-editor#install*/}
+        <Button>Submit new issue</Button>
+      </form>
+    </div>
   );
 };
 
